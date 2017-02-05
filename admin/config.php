a42c43ea33e5984363778611db1e6ba670f80b69
Beattle <ea29db6e18>
2017-02-05 19:12:36 +0300

    Initial commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://shop.oc-project.ru/krok/admin/');
+define('HTTP_CATALOG', 'http://shop.oc-project.ru/krok/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://shop.oc-project.ru/krok/admin/');
+define('HTTPS_CATALOG', 'http://shop.oc-project.ru/krok/');
+
+// DIR
+define('DIR_APPLICATION', '/srv/hipno/html/test.com/krok/admin/');
+define('DIR_SYSTEM', '/srv/hipno/html/test.com/krok/system/');
+define('DIR_IMAGE', '/srv/hipno/html/test.com/krok/image/');
+define('DIR_LANGUAGE', '/srv/hipno/html/test.com/krok/admin/language/');
+define('DIR_TEMPLATE', '/srv/hipno/html/test.com/krok/admin/view/template/');
+define('DIR_CONFIG', '/srv/hipno/html/test.com/krok/system/config/');
+define('DIR_CACHE', '/srv/hipno/html/test.com/krok/system/storage/cache/');
+define('DIR_DOWNLOAD', '/srv/hipno/html/test.com/krok/system/storage/download/');
+define('DIR_LOGS', '/srv/hipno/html/test.com/krok/system/storage/logs/');
+define('DIR_MODIFICATION', '/srv/hipno/html/test.com/krok/system/storage/modification/');
+define('DIR_UPLOAD', '/srv/hipno/html/test.com/krok/system/storage/upload/');
+define('DIR_CATALOG', '/srv/hipno/html/test.com/krok/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'hipno');
+define('DB_PASSWORD', 'HYsfp5gY6v0rhDEOJ2r8');
+define('DB_DATABASE', 'krok');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://shop.oc-project.ru/krok/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://shop.oc-project.ru/krok/');
+
+// DIR
+define('DIR_APPLICATION', '/srv/hipno/html/test.com/krok/catalog/');
+define('DIR_SYSTEM', '/srv/hipno/html/test.com/krok/system/');
+define('DIR_IMAGE', '/srv/hipno/html/test.com/krok/image/');
+define('DIR_LANGUAGE', '/srv/hipno/html/test.com/krok/catalog/language/');
+define('DIR_TEMPLATE', '/srv/hipno/html/test.com/krok/catalog/view/theme/');
+define('DIR_CONFIG', '/srv/hipno/html/test.com/krok/system/config/');
+define('DIR_CACHE', '/srv/hipno/html/test.com/krok/system/storage/cache/');
+define('DIR_DOWNLOAD', '/srv/hipno/html/test.com/krok/system/storage/download/');
+define('DIR_LOGS', '/srv/hipno/html/test.com/krok/system/storage/logs/');
+define('DIR_MODIFICATION', '/srv/hipno/html/test.com/krok/system/storage/modification/');
+define('DIR_UPLOAD', '/srv/hipno/html/test.com/krok/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'hipno');
+define('DB_PASSWORD', 'HYsfp5gY6v0rhDEOJ2r8');
+define('DB_DATABASE', 'krok');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
